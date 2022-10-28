3ab40318635412fb4b1f04a0f4314617c56c23e9
Halicrea <elyna.bouchereau@outlook.fr>
2022-10-28 16:53:02 +0200

    Moi ce que j'aime dans les balades, c'est qu'on peut se balader 🎵

--- main.cpp
+++ main.cpp
@@ -17,9 +17,19 @@ int main()
     cin >> mdp;
 
     //lire le fichier bdd_patients_medecins.txt pour y vérifier la véracité des informations entrées
-    ifstream file;
-    file.open("bdd_patients_medecins.txt");
-    
+    string mon_fichier = "bdd_patients_medecins.txt";  // je stocke dans la chaîne mon_fichier le nom du fichier à ouvrir
+     ifstream fichier(mon_fichier.c_str(), ios::in);
+     if(fichier)  // si l'ouverture a réussi
+     {
+        string ligne;  // déclaration d'une chaîne qui contiendra la ligne lue
+        while(getline(fichier, ligne))  // tant que l'on peut mettre la ligne dans "contenu"
+        {
+                cout << ligne << endl;  // on l'affiche
+        }
+
+        fichier.close();  // je referme le fichier
+     }
+
     if (id == "a01" && mdp == "miRNA")
     {
         cout << "vous pouvez consulter :)"
